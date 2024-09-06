30b1fae578c540b7c2f81b5573c10d9535706d5b
sqsakchyam <sakchyam314@gmail.com>
2024-09-06 16:19:53 +0545

    add

---
+++ data_pack (1).sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 06, 2024 at 12:28 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `data_pack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `combined`
+--
+-- Error reading structure for table data_pack.combined: #1932 - Table 'data_pack.combined' doesn't exist in engine
+-- Error reading data for table data_pack.combined: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`combined`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datapacks`
+--
+-- Error reading structure for table data_pack.datapacks: #1932 - Table 'data_pack.datapacks' doesn't exist in engine
+-- Error reading data for table data_pack.datapacks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`datapacks`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+-- Error reading structure for table data_pack.districts: #1932 - Table 'data_pack.districts' doesn't exist in engine
+-- Error reading data for table data_pack.districts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`districts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kyc_confirmation`
+--
+-- Error reading structure for table data_pack.kyc_confirmation: #1932 - Table 'data_pack.kyc_confirmation' doesn't exist in engine
+-- Error reading data for table data_pack.kyc_confirmation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`kyc_confirmation`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `municipality`
+--
+-- Error reading structure for table data_pack.municipality: #1932 - Table 'data_pack.municipality' doesn't exist in engine
+-- Error reading data for table data_pack.municipality: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`municipality`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinces`
+--
+-- Error reading structure for table data_pack.provinces: #1932 - Table 'data_pack.provinces' doesn't exist in engine
+-- Error reading data for table data_pack.provinces: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`provinces`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+-- Error reading structure for table data_pack.test: #1932 - Table 'data_pack.test' doesn't exist in engine
+-- Error reading data for table data_pack.test: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`test`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test2`
+--
+-- Error reading structure for table data_pack.test2: #1932 - Table 'data_pack.test2' doesn't exist in engine
+-- Error reading data for table data_pack.test2: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`test2`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+-- Error reading structure for table data_pack.user_info: #1932 - Table 'data_pack.user_info' doesn't exist in engine
+-- Error reading data for table data_pack.user_info: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `data_pack`.`user_info`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
